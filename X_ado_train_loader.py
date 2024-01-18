7fa8126599d6bdbd2d8c62c42d542e7385a12040
jyang297 <jyang297@uottawa.ca>
2024-01-18 18:25:09 -0500

    correct loader and add .gitignore

--- X_ado_train_loader.py
+++ X_ado_train_loader.py
@@ -1,17 +1,29 @@
 import os
+import numpy as np
 from PIL import Image
 import torch
-import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
+def crop(img, h, w):
+    img_np = np.array(img)  # Convert PIL Image to numpy array
+    ih, iw, _ = img_np.shape
+    x = np.random.randint(0, ih - h + 1)
+    y = np.random.randint(0, iw - w + 1)
+    img_cropped = img_np[x:x+h, y:y+w, :]
+    return Image.fromarray(img_cropped)  # Convert back to PIL Image
+
+# Define transformations
+transform = transforms.Compose([
+    transforms.ToTensor()  # Convert images to tensor after all other transformations
+])
+
 class X4K1000FPSDataset(Dataset):
-    def __init__(self, root_dir, transform=None, crop=None, ):
+    def __init__(self, root_dir, transform=transform, crop_size=None):
         self.root_dir = root_dir
-        self.crop = crop
         self.transform = transform
+        self.crop_size = crop_size
         self.image_groups = []
-        self.transformer = transformImages()
 
         for subdir in os.listdir(root_dir):
             subdir_path = os.path.join(root_dir, subdir)
@@ -28,63 +40,17 @@ class X4K1000FPSDataset(Dataset):
     def __getitem__(self, idx):
         image_paths = self.image_groups[idx]
         images = [Image.open(img_path).convert('RGB') for img_path in image_paths]
+
+        if self.crop_size is not None:
+            h, w = self.crop_size
+            images = [crop(image, h, w) for image in images]
+
         images = [self.transform(image) for image in images]
-
-
-        if self.transform:
-            for image in images:
-                if self.crop:
-                    image = self.transformImages()
-            
-
         image_stack = torch.stack(images, dim=0)
         return image_stack
 
-# Define transformations, if necessary
-transform = transforms.Compose([
-    transforms.ToTensor(),
-])
 # Create the dataset
-dataset = X4K1000FPSDataset(root_dir='/home/jyzhao/Code/Datasets/X4K1000FPS', transform=transform)
+dataset = X4K1000FPSDataset(root_dir='/home/jyzhao/Code/Datasets/X4K1000FPS', transform=transform, crop_size=(224, 224))
 
 # Create the DataLoader
 dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
-
-# Now you can use this dataloader in your training loop
-
-class transformImages():
-    def crop(self, img0, gt, img1, h, w):
-        ih, iw, _ = img0.shape
-        x = np.random.randint(0, ih - h + 1)
-        y = np.random.randint(0, iw - w + 1)
-        img0 = img0[x:x+h, y:y+w, :]
-        # img1 = img1[x:x+h, y:y+w, :]
-        # gt = gt[x:x+h, y:y+w, :]
-        return img0
-    
-    
-    def RGB2Gray(image, channel=3):
-    ## input : T, H, W, C
-
-        # rgb --> Y (gray)
-        r, g, b = image[0], image[1], image[2]
-        gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
-
-        # Add channel dimension
-        gray = gray.unsqueeze(0)
-        return gray
-    def normalization(image, range=None):
-        if range:
-            # normalization [-1,1]
-            imgIn = (imgIn / 255.0 - 0.5) * 2
-        else:
-            # normalization [0,1]
-            imgIn = (imgIn / 255.0)
-    
-        return imgIn
-
-def NGNtripletSetup(_):
-    return None
-
-
- 
